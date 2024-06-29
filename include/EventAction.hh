df7a164bcda7bc50673f0a761cf077d32215cd84
Simone Mariottini <mariottini.1893631@studenti.uniroma1.it>
2024-06-29 15:25:01 +0200

    Reapply "Add old"
    This reverts commit 5ed15c8dbcb867f1ee9bf20ff21a4739b6c35d06.

---
+++ include/EventAction.hh
@@ -0,0 +1,114 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// \file B4/B4a/include/EventAction.hh
+/// \brief Definition of the B4a::EventAction class
+
+#ifndef EventAction_h
+#define EventAction_h 1
+
+#include "G4UserEventAction.hh"
+#include "globals.hh"
+
+namespace brooklyn2024
+{
+  #define NLAYERS 16
+
+/// Event action class
+///
+/// It defines data members to hold the energy deposit and track lengths
+/// of charged particles in Absober and Gap layers:
+/// - fEnergyAbs, fEnergyGap, fTrackLAbs, fTrackLGap
+/// which are collected step by step via the functions
+/// - AddAbs(), AddGap()
+
+class EventAction : public G4UserEventAction
+{
+  public:
+    EventAction() = default;
+    ~EventAction() override = default;
+
+    void   BeginOfEventAction(const G4Event* event) override;
+    void   EndOfEventAction(const G4Event* event) override;
+
+    void AddLayer(G4int i, G4double de, G4double dl);
+
+  private:
+    G4double  fEnergyFibers[NLAYERS] = {0.};
+    G4double  fTrackLFibers[NLAYERS] = {0.};
+    G4double  fEnergyTot = 0.;
+    G4double  fTrackLTot = 0.;
+};
+
+// inline functions
+
+inline void EventAction::AddLayer(G4int i, G4double de, G4double dl) {
+  fEnergyFibers[i] += de;
+  fTrackLFibers[i] += dl;
+  fEnergyTot += de;
+  fTrackLTot += dl;
+}
+
+}
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+namespace brooklyn2024Acceptance
+{
+  #define NLAYERS 16
+
+/// Event action class
+///
+/// It defines data members to hold the energy deposit and track lengths
+/// of charged particles in Absober and Gap layers:
+/// - fEnergyAbs, fEnergyGap, fTrackLAbs, fTrackLGap
+/// which are collected step by step via the functions
+/// - AddAbs(), AddGap()
+
+class EventAction : public G4UserEventAction
+{
+  public:
+    EventAction() = default;
+    ~EventAction() override = default;
+
+    void   BeginOfEventAction(const G4Event* event) override;
+    void   EndOfEventAction(const G4Event* event) override;
+
+    G4bool const GetEntered() {return fEntered;}
+    G4bool const GetExited() {return fExited;}
+
+    void SetEntered(G4bool check) {fEntered = check;}
+    void SetExited(G4bool check) {fExited = check;}
+
+  private:
+    G4bool fEntered = false;
+    G4bool fExited = false;
+};
+
+}
+
+#endif
+
+
---
+++ src/RunAction.cc
@@ -0,0 +1,199 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// \file B4/B4a/src/RunAction.cc
+/// \brief Implementation of the B4::RunAction class
+
+#include "RunAction.hh"
+
+#include "G4AnalysisManager.hh"
+#include "G4Run.hh"
+#include "G4RunManager.hh"
+#include "G4UnitsTable.hh"
+#include "G4SystemOfUnits.hh"
+
+namespace brooklyn2024
+{
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+RunAction::RunAction()
+{
+  // set printing event number per each event
+  G4RunManager::GetRunManager()->SetPrintProgress(1);
+
+  // Create analysis manager
+  // The choice of the output format is done via the specified
+  // file extension.
+  auto analysisManager = G4AnalysisManager::Instance();
+
+  // Create directories
+  //analysisManager->SetHistoDirectoryName("histograms");
+  //analysisManager->SetNtupleDirectoryName("ntuple");
+  analysisManager->SetVerboseLevel(1);
+  analysisManager->SetNtupleMerging(true);
+    // Note: merging ntuples is available only with Root output
+
+  // Book histograms, ntuple
+  //
+
+  // Creating histograms
+  analysisManager->CreateH1("Etot" ,"Edep in hodoscope", 100, 0., 0.050*MeV);
+  analysisManager->CreateH1("Ltot" ,"trackL in hodoscope", 100, 0., 50*cm);
+  
+  // Creating ntuple
+  analysisManager->CreateNtuple("Brooklyn2024", "Edep and TrackL");
+  analysisManager->CreateNtupleDColumn("Etot");
+  analysisManager->CreateNtupleDColumn("Ltot");
+
+  for(int i = 0; i < NLAYERS; i++){
+    analysisManager->CreateH1("E_" + to_string(i) ,"Edep in fiber layer " + to_string(i), 100, 0., 0.010*MeV);
+    analysisManager->CreateH1("L_" + to_string(i) ,"trackL in fiber layer" + to_string(i), 100, 0., 50*cm);
+
+    analysisManager->CreateNtupleDColumn("E_" + to_string(i));
+    analysisManager->CreateNtupleDColumn("L_" + to_string(i));
+  }
+
+  analysisManager->FinishNtuple();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void RunAction::BeginOfRunAction(const G4Run* /*run*/)
+{
+  //inform the runManager to save random number seed
+  //G4RunManager::GetRunManager()->SetRandomNumberStore(true);
+
+  // Get analysis manager
+  auto analysisManager = G4AnalysisManager::Instance();
+
+  // Open an output file
+  //
+  G4String fileName = "brooklyn2024.root";
+
+  analysisManager->OpenFile(fileName);
+  G4cout << "Using " << analysisManager->GetType() << G4endl;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void RunAction::EndOfRunAction(const G4Run* /*run*/)
+{
+  // print histogram statistics
+  //
+  auto analysisManager = G4AnalysisManager::Instance();
+  if ( analysisManager->GetH1(1) ) {
+    G4cout << G4endl << " ----> print histograms statistic ";
+    if(isMaster) {
+      G4cout << "for the entire run " << G4endl << G4endl;
+    }
+    else {
+      G4cout << "for the local thread " << G4endl << G4endl;
+    }
+
+    G4int i = 0;
+
+    G4cout << " Etot : mean = "
+       << G4BestUnit(analysisManager->GetH1(i)->mean(), "Energy")
+       << " rms = "
+       << G4BestUnit(analysisManager->GetH1(i)->rms(),  "Energy") << G4endl;
+
+    i++;
+    G4cout << " Ltot : mean = "
+      << G4BestUnit(analysisManager->GetH1(i)->mean(), "Length")
+      << " rms = "
+      << G4BestUnit(analysisManager->GetH1(i)->rms(),  "Length") << G4endl; 
+
+      for(int j = 0; j < NLAYERS; j++){
+        i++;
+        G4cout << " E " + to_string(j) + " : mean = "
+          << G4BestUnit(analysisManager->GetH1(i)->mean(), "Energy")
+          << " rms = "
+          << G4BestUnit(analysisManager->GetH1(i)->rms(),  "Energy") << G4endl;
+
+        i++;
+        G4cout << " L " + to_string(j) + " : mean = "
+          << G4BestUnit(analysisManager->GetH1(i)->mean(), "Length")
+          << " rms = "
+          << G4BestUnit(analysisManager->GetH1(i)->rms(),  "Length") << G4endl; 
+      }    
+  }
+
+  // save histograms & ntuple
+  //
+  analysisManager->Write();
+  analysisManager->CloseFile();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+}
+
+namespace brooklyn2024Acceptance
+{
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+RunAction::RunAction()
+{
+  // set printing event number per each event
+  G4RunManager::GetRunManager()->SetPrintProgress(1);
+
+  auto analysisManager = G4AnalysisManager::Instance();
+
+  analysisManager->SetVerboseLevel(1);
+  analysisManager->SetNtupleMerging(true);
+
+  analysisManager->CreateH1("Acceptance" ,"Entered and exited", 2, -0.5, 1.5);
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void RunAction::BeginOfRunAction(const G4Run* /*run*/)
+{
+  // Get analysis manager
+  auto analysisManager = G4AnalysisManager::Instance();
+
+  // Open an output file
+  //
+  G4String fileName = "brooklyn2024Acceptance.root";
+
+  analysisManager->OpenFile(fileName);
+  G4cout << "Using " << analysisManager->GetType() << G4endl;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void RunAction::EndOfRunAction(const G4Run* /*run*/)
+{
+  auto analysisManager = G4AnalysisManager::Instance();
+  analysisManager->Write();
+  analysisManager->CloseFile();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+}
